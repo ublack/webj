1f52d470ffb441e03c4d4a1233c1474b6ad86254
ublack <lyq1747752310@127.com>
2021-02-06 00:12:40 +0800

    输出播放地址

--- src/main/java/com/webj/util/TvUpdate.java
+++ src/main/java/com/webj/util/TvUpdate.java
@@ -2,13 +2,9 @@ package com.webj.util;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.springframework.util.FileCopyUtils;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TvUpdate {
 
@@ -16,28 +12,29 @@ public class TvUpdate {
 
         String mp = System.getProperty("user.home");
 
-        HashMap<String, String> ees = new LinkedHashMap<>();
+        HashMap<String, List<String>> ees = new LinkedHashMap<>();
 
-        ees.put("一念永恒", "https://v.qq.com/detail/w/ww18u675tfmhas6.html");
-        ees.put("仙风剑雨录", "https://v.qq.com/detail/m/mzc00200hc38s5x.html");
-        ees.put("无上神帝", "https://v.qq.com/detail/m/mzc00200ilydv1a.html");
-        ees.put("灵剑尊", "https://v.qq.com/detail/2/2w2legt0g8z26al.html");
-        ees.put("斗罗大陆", "https://v.qq.com/detail/m/m441e3rjq9kwpsc.html");
-        ees.put("武神主宰", "https://v.qq.com/detail/7/7q544xyrava3vxf.html");
+        ees.put("一念永恒", Arrays.asList("https://v.qq.com/detail/w/ww18u675tfmhas6.html", "http://www.tadedy.com/play/50502-0-@@@.html"));
+        ees.put("仙风剑雨录", Arrays.asList("https://v.qq.com/detail/m/mzc00200hc38s5x.html", "http://www.tadedy.com/play/49430-@@@-0.html"));
+        ees.put("灵剑尊", Arrays.asList("https://v.qq.com/detail/2/2w2legt0g8z26al.html", "http://www.tadedy.com/play/28274-0-@@@.html"));
+        ees.put("斗罗大陆", Arrays.asList("https://v.qq.com/detail/m/m441e3rjq9kwpsc.html", "http://www.tadedy.com/play/20358-0-@@@.html"));
+        ees.put("武神主宰", Arrays.asList("https://v.qq.com/detail/7/7q544xyrava3vxf.html", "http://www.tadedy.com/play/42462-0-@@@.html"));
+        ees.put("万界法神", Arrays.asList("https://v.qq.com/detail/m/mzc002007995z4v.html", "http://www.tadedy.com/play/56155-0-@@@.html"));
+        ees.put("万界仙踪", Arrays.asList("https://v.qq.com/detail/5/5cuf8ahvxvm2587.html", "http://www.tadedy.com/play/21976-0-@@@.html"));
 
-        StringBuilder mem = new StringBuilder();
+//        StringBuilder mem = new StringBuilder();
 
-        for (Map.Entry<String, String> entry : ees.entrySet()) {
-            Document doc = Jsoup.connect(entry.getValue()).get();
-            String t1 = String.format("%s  %s%n", entry.getKey(), doc.select("[itemprop=episodeNumber]").last().text());
+        for (Map.Entry<String, List<String>> entry : ees.entrySet()) {
+            Document doc = Jsoup.connect(entry.getValue().get(0)).get();
+            String episodeNumber = doc.select("[itemprop=episodeNumber]").last().text();
+            String t1 = String.format("%s  %s  %s%n", entry.getKey(), episodeNumber, entry.getValue().get(1).replaceFirst("@@@", String.valueOf(Integer.parseInt(episodeNumber) - 1)));
+
             String t2 = String.format("%s%n", doc.select(".video_type_even").first().select(".type_txt").text());
-            mem.append(t1).append(t2).append(System.lineSeparator());
+//            mem.append(t1).append(t2).append(System.lineSeparator());
             System.out.println(t1);
             System.out.println(t2);
         }
-        FileCopyUtils.copy(mem.toString().getBytes(),
-                new File(mp + "\\IdeaProjects\\webj\\src\\main\\resources\\mem.txt"));
-
-
+//        FileCopyUtils.copy(mem.toString().getBytes(),
+//                new File(mp + "\\IdeaProjects\\webj\\src\\main\\resources\\mem.txt"));
     }
 }
